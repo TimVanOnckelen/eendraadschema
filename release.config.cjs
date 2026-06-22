ac673ec1118fb8b20af52b8b2c9c7e931a3845c3
Tim Van Onckelen <tim@xeweb.be>
2026-06-22 17:16:59 +0200

    chore: update release workflow and configuration, remove unused assets

---
+++ release.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('semantic-release').GlobalConfig} */
+module.exports = {
+  branches: ['master', 'main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/npm', { npmPublish: false }],
+    '@semantic-release/github',
+  ],
+};
